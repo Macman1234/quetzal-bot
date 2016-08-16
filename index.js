393ba0cf50b263ba2c300bd56d55ea5f6b48c2ee
Maclean Parisi <ninjamurloc@gmail.com>
2016-08-16 11:01:01 -0400

    Added in Youtube search function.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var cool = require('cool-ascii-faces');
 var Discord = require("discord.js");
+var YouTube = require('youtube-node');
 
 var bot = new Discord.Client();
+
+var youtubeClient = new YouTube();
+
+youtubeClient.setKey('AIzaSyCdOuxh7jgLDXUxaM630eU4jx-MybF76q0');
 
 //when the bot is ready
 bot.on("ready", () => {
@@ -28,6 +33,15 @@ bot.on("message", function(msg) {
         bot.sendMessage(msg.channel, "polo");
     } else if (msg.content === "face") {
         bot.sendMessage(msg.channel, cool());
+    } else if (msg.content.startsWith("youtube")) {
+        var searchterm = msg.content.split(" ").slice(1).join(" ");
+        youtubeClient.search(searchterm, 2, function(error, result) {
+            if (error) {
+                bot.sendMessage(msg.channel, error);
+            } else {
+                bot.sendMessage(msg.channel, "https://youtu.be/" + result.items[0].id.videoId);
+            }
+        });
     }
 });
 
