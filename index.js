21b584b072bbdeb56251eb0ea8cabac1fc851ea2
Maclean Parisi <ninjamurloc@gmail.com>
2016-08-15 21:31:32 -0400

    !s didnt work

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var Discord = require("discord.js");
 var mybot = new Discord.Client();
 
 mybot.on("message", function(message) {
-    if(message.content === "!ping") {
+    if(message.content === "ping") {
         mybot.sendMessage(message.channel, "pong");
     }
-    if(message.content === "!face") {
+    if(message.content === "face") {
         mybot.sendMessage(message.channel, cool());
     }
 });
