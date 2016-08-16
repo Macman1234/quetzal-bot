6cbcd030192670d30a64fd578b497ed5335f13b7
Maclean Parisi <ninjamurloc@gmail.com>
2016-08-16 09:55:37 -0400

    added some logging, changed the way commands are read

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 var cool = require('cool-ascii-faces');
 var Discord = require("discord.js");
 
-var mybot = new Discord.Client();
+var bot = new Discord.Client();
 
-mybot.on("message", function(message) {
-    if(message.content === "ping") {
-        mybot.sendMessage(message.channel, "pong");
-    }
-    if(message.content === "face") {
-        mybot.sendMessage(message.channel, cool());
+//when the bot is ready
+bot.on("ready", () => {
+    console.log(`Ready to begin! Serving in ${bot.channels.length} channels`);
+});
+
+bot.on("disconnected", () => {
+    //alert the console
+    console.log("Disconnected!");
+
+    //exit node.js with an error
+    process.exit(1);
+});
+
+
+bot.on("message", function(msg) {
+    if (!msg.content.startsWith("!")) return;
+
+    msg.content = msg.content.substr(1);
+
+    if (msg.content === "ping") {
+        bot.sendMessage(msg.channel, "pong");
+    } else if (msg.content === "pong") {
+        bot.sendMessage(msg.channel, "polo");
+    } else if (msg.content === "face") {
+        bot.sendMessage(msg.channel, cool());
     }
 });
 
-mybot.loginWithToken("MjE0ODI2NDA3ODEyMDA1OTA4.CpPzww.WQAVrwaHbTlqiLiOFankkJYPhwo");
+bot.loginWithToken("MjE0ODI2NDA3ODEyMDA1OTA4.CpPzww.WQAVrwaHbTlqiLiOFankkJYPhwo");
