70e94bb82acc5b4d677d417987332def53daccf3
Maclean Parisi <ninjamurloc@gmail.com>
2016-08-15 21:21:22 -0400

    removed replying, replaced with sendmessage into channel

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ var mybot = new Discord.Client();
 
 mybot.on("message", function(message) {
     if(message.content === "ping") {
-        mybot.reply(message, "pong");
+        mybot.sendMessage(message.channel, "pong");
+    }
+    if(message.content === "face") {
+        mybot.sendMessage(message.channel, cool());
     }
 });
+
+process.on('SIGINT', function () {
+    console.log('Ctrl-C...');
+    mybot.destroy();
+  });
 
 mybot.loginWithToken("MjE0ODI2NDA3ODEyMDA1OTA4.CpPzww.WQAVrwaHbTlqiLiOFankkJYPhwo");
